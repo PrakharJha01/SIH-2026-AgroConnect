6846c28d741b633c7e8327306a786d6f33e1d717
Prakhar Jha <141326503+PrakharJha01@users.noreply.github.com>
2026-09-02 17:50:32 +0530

    Delete check_logistics.py

--- check_logistics.py
+++
@@ -1,9 +0,0 @@
-﻿with open('pages/23_Buyer_Matches.py', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-import re
-matches = re.search(r'def _render_logistics_map', text)
-if matches:
-    print("Found map")
-else:
-    print("No map")
